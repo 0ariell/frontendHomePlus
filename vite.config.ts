ebc0bd470ada50ea22b7703c54b2cc6d02392c17
0ariell <arlmp135@gmail.com>
2025-05-16 00:24:33 -0300

    � add TSConfig + alias + excluir backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -5,16 +6,11 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // que '@/...' sea equivalente a '<projectRoot>/src/...'
-      "@": path.resolve(__dirname, "src"),
-    },
+    alias: { "@": path.resolve(__dirname, "src") },
   },
   server: {
     host: true,
     port: 5173,
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
+    proxy: { "/api": "http://localhost:3000" },
   },
 });
